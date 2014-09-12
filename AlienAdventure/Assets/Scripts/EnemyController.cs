4f2157600162cf807d329558811e4913206f98ce
Anderson Birocchi <anderson.birocchi@gmail.com>
2014-09-12 00:51:38 -0300

    Novo inimigo! A mosca!!!

--- AlienAdventure/Assets/Scripts/EnemyController.cs
+++ AlienAdventure/Assets/Scripts/EnemyController.cs
@@ -15,6 +15,7 @@ public class EnemyController: MonoBehaviour {
 	Vector3 initialPosition;
 	Direction direction = Direction.left;
 	Animator animator;
+	bool dead = false;
 
 	void Start(){
 		animator = GetComponent<Animator>();
@@ -23,13 +24,15 @@ public class EnemyController: MonoBehaviour {
 	}
 
 	void Update () {
-		if(transform.position.x <= initialPosition.x + minWalkLimit){
-			direction = Direction.right;
-			transform.eulerAngles = new Vector3(0,180,0);
-		}
-		else if(transform.position.x >= initialPosition.x + maxWalkLimit){
-			direction = Direction.left;
-			transform.eulerAngles = new Vector3(0,0,0);
+		if (!dead){
+			if(transform.position.x <= initialPosition.x + minWalkLimit){
+				direction = Direction.right;
+				transform.eulerAngles = new Vector3(0,180,0);
+			}
+			else if(transform.position.x >= initialPosition.x + maxWalkLimit){
+				direction = Direction.left;
+				transform.eulerAngles = new Vector3(0,0,0);
+			}
 		}
 	}
 	
@@ -38,13 +41,14 @@ public class EnemyController: MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
-		if(other.gameObject.tag == "Player" && other.contacts[0].normal.y < -0.9){
-			animator.SetBool("Dead", true);
-			rigidbody2D.isKinematic = true;
+		if(other.gameObject.tag == "Player" && other.contacts[0].normal.y < -0.8){
+			Physics2D.IgnoreCollision (other.collider, this.collider2D);
+			if(!dead)
+				audio.Play();
+			dead = true;
 			speed = 0;
+			animator.SetBool("Dead", true);
 			scoreManager.AddScore(50);
-			audio.Play();
-			Destroy(collider2D);
 			Destroy(gameObject,5);
 		}
 	}
