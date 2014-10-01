3ef6e41cae7cb2641f8a8770aff147f554a6d976
Anderson Birocchi <anderson.birocchi@gmail.com>
2014-10-01 01:23:32 -0300

    Agora os inimigos podem se movimentar na vertical

--- AlienAdventure/Assets/Scripts/EnemyController.cs
+++ AlienAdventure/Assets/Scripts/EnemyController.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class EnemyController: MonoBehaviour {
 	
-	enum Direction {left = -1, right = 1};
+	enum Direction {left = -1, right = 1, down = -1, up = 1, none = 0};
 	
 	public int scoreValue;
 	public int damage;
 	public float speed;
-	public float minWalkLimit;
-	public float maxWalkLimit;
+	public Vector2 minLimit = new Vector2(-1,-1);
+	public Vector2 maxLimit = new Vector2(1,1);
+	public Vector2 initialDirection;
 	public Sprite deathSprite;
 
 	ScoreManager scoreManager;
-
-	Vector3 initialPosition;
-	Direction direction = Direction.left;
+	Vector2 initialPosition;
+	Vector2 direction;
 	Animator animator;
 	bool dead = false;
 
@@ -23,23 +23,38 @@ public class EnemyController: MonoBehaviour {
 		animator = GetComponent<Animator>();
 		scoreManager = GameObject.Find("GameManager").GetComponent<ScoreManager>();
 		initialPosition = transform.position;
+		direction = initialDirection;
+		if(initialDirection.x == (int)Direction.right) 
+			transform.eulerAngles = new Vector3(0,180,0);
 	}
 
 	void Update () {
 		if (!dead){
-			if(transform.position.x <= initialPosition.x + minWalkLimit){
-				direction = Direction.right;
+			if(transform.position.x <= initialPosition.x + minLimit.x){
+				direction.x = (int)Direction.right;
 				transform.eulerAngles = new Vector3(0,180,0);
 			}
-			else if(transform.position.x >= initialPosition.x + maxWalkLimit){
-				direction = Direction.left;
+			else if(transform.position.x >= initialPosition.x + maxLimit.x){
+				direction.x = (int)Direction.left;
 				transform.eulerAngles = new Vector3(0,0,0);
+			}
+
+			if(transform.position.y <= initialPosition.y + minLimit.y){
+				direction.y = (int)Direction.up;
+			}
+			else if(transform.position.y >= initialPosition.y + maxLimit.y){
+				direction.y = (int)Direction.down;
 			}
 		}
 	}
 	
 	void FixedUpdate () {
-		rigidbody2D.velocity = new Vector2((int)direction * speed, rigidbody2D.velocity.y);
+		if(!dead){
+			rigidbody2D.velocity = new Vector2((int)direction.x * speed, (int)direction.y * speed);
+		}
+		else{
+			rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
@@ -49,13 +64,11 @@ public class EnemyController: MonoBehaviour {
 				Physics2D.IgnoreCollision (other.collider, this.collider2D);
 				audio.Play();
 				dead = true;
-				speed = 0;
 				animator.SetBool("Dead", true);
 				scoreManager.AddScore(scoreValue);
 				Destroy(gameObject,2);
 			}
 			else {
-
 				other.rigidbody.velocity = (-other.contacts[0].normal + Vector2.up) * 3;
 				other.gameObject.GetComponent<PlayerController>().HurtPlayer(damage);
 
