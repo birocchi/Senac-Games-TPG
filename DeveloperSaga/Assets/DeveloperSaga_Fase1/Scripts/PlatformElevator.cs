487aef35b49c8e3f8e38954e9a65f6030425010c
lfadelli <leandro.fadelli@hotmail.com>
2014-10-01 19:15:29 -0300

    Melhorias na plataforma móvel

--- DeveloperSaga/Assets/DeveloperSaga_Fase1/Scripts/PlatformElevator.cs
+++ DeveloperSaga/Assets/DeveloperSaga_Fase1/Scripts/PlatformElevator.cs
@@ -3,52 +3,54 @@ using System.Collections;
 
 public class PlatformElevator : MonoBehaviour {
 	private bool elevatorOn;
-	private bool goingUp;
-	public float initialPosition;
+	private bool goUp;
+	private float initialPosition;
 	public float maxPosition;
-
+	
 	// Use this for initialization
 	void Start () {
 		initialPosition = transform.position.y;
-		maxPosition = initialPosition + 5;
+		if (maxPosition == 0) {
+			maxPosition = initialPosition + 5;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void FixedUpdate () {
-		if (elevatorOn) {
-			if(this.transform.position.y >= maxPosition) {
-				goingUp = false;
-			}
-			if(this.transform.position.y <= initialPosition) {
-				goingUp = true;
-			}
-
-			if (goingUp) {
-				this.transform.Translate (0, 0.05f, 0);
-			}
-			if (goingUp == false) {
-				this.transform.Translate (0, -0.05f, 0);
-			}
+		bool startPoint = false;
+		
+		if(this.transform.position.y >= maxPosition) {
+			goUp = false;
+		}
+		if (this.transform.position.y <= initialPosition) {
+			goUp = true;
+			startPoint = true;
+		}
+		
+		if (startPoint && !elevatorOn) {
+			return;
+		}
+		
+		if (goUp == false) {
+			this.transform.Translate (0, -0.05f, 0);
+		} else {
+			this.transform.Translate (0, 0.05f, 0);
 		}
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag.Equals ("Player")) {
 			elevatorOn = true;
 		}
 	}
-
-	void OnCollisionExit2d(Collision2D collision){
+	
+	void OnCollisionExit2D(Collision2D collision){
 		if (collision.gameObject.tag.Equals ("Player")) {
 			elevatorOn = false;
 		}
-	}
-
-	void GoUp(){
-		rigidbody2D.AddForce(Vector3.up * 1000);
 	}
 }
