e49234895c7ccaf3123c565491d82738baa2a612
lfadelli <leandro.fadelli@hotmail.com>
2014-10-03 19:30:05 -0300

    Animação Preliminar de Script

--- DeveloperSaga/Assets/DeveloperSaga_Fase1/Scripts/RockTrigger.cs
+++ DeveloperSaga/Assets/DeveloperSaga_Fase1/Scripts/RockTrigger.cs
@@ -6,10 +6,11 @@ public class RockTrigger : MonoBehaviour {
 
 	private void ReleaseRock() {
 		Rock.rigidbody2D.isKinematic = false;
+		this.collider2D.enabled = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag.Equals ("Player")) {
+		if (other.tag.Equals ("Player")  ) {
 			Debug.Log ("Soltar Pedra!");
 			this.ReleaseRock ();
 		}
