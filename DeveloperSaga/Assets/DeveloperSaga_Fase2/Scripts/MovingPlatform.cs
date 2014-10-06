f93214f1d20e79cea230901847ad14f4a07aa71e
Anderson Birocchi <anderson.birocchi@gmail.com>
2014-10-05 22:52:37 -0300

    Agora é possível determinar visualmente até onde a plataforma vai se movimentar, através do editor

--- DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/MovingPlatform.cs
+++ DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/MovingPlatform.cs
@@ -3,29 +3,53 @@ using System.Collections;
 
 public class MovingPlatform : MonoBehaviour {
 
-	public Vector2 movingDirection;
+	public Transform destination;
 	public float speed = 1;
-	public float movingTime = 1;
 
-	private float elapsedTime;
-	private Rigidbody2D standingBody;
+	private Vector3 initialPosition;
+	private Vector3 finalPosition;
+	private Vector2 movingDirection;
+	private bool waiting;
 
 	void Start() {
-		elapsedTime = 0;
-		movingDirection = movingDirection.normalized;
+		waiting = false;
+		initialPosition = transform.position;
+		finalPosition = destination.position;
+		movingDirection = (finalPosition - initialPosition).normalized;
 	}
 
 	void FixedUpdate() {
 
-		if(elapsedTime < movingTime){
+		if(Vector3.Distance(transform.position, finalPosition) > 0.01f){
 			rigidbody2D.velocity = movingDirection * speed;
-			elapsedTime += Time.fixedDeltaTime;
 		}
-		else {
-			speed = -speed;
-			elapsedTime = 0;
+		else if(!waiting){
+			rigidbody2D.velocity = Vector2.zero;
+			StartCoroutine(ChangeDirection(1));
 		}
-
 	}
 
+	IEnumerator ChangeDirection(float seconds){
+		waiting = true;
+
+		yield return new WaitForSeconds(seconds);
+
+		Vector3 tempSwap = finalPosition;
+		finalPosition = initialPosition;
+		initialPosition = tempSwap;
+
+		movingDirection = -movingDirection;
+
+		waiting = false;
+	}
+
+	void OnDrawGizmos(){
+		if(finalPosition.Equals(Vector3.zero) && initialPosition.Equals(Vector3.zero)){
+			Gizmos.DrawWireCube(destination.position + new Vector3(0,0.15f,0) , new Vector3(2.1f, 0.4f, 0f));
+		}
+		else {
+			Gizmos.DrawWireCube(initialPosition + new Vector3(0,0.15f,0) , new Vector3(2.1f, 0.4f, 0f));
+			Gizmos.DrawWireCube(finalPosition + new Vector3(0,0.15f,0) , new Vector3(2.1f, 0.4f, 0f));
+		}
+	}
 }
