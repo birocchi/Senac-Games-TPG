3d6566bfa3dfd9559d967e32d631f7d6e8ffedd1
Anderson Birocchi <anderson.birocchi@gmail.com>
2014-10-09 08:14:54 -0300

    Chaves começando a serem criadas

---
+++ DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/GetKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetKey : MonoBehaviour {
+
+	KeysManager keyManager;
+
+	// Use this for initialization
+	void Start () {
+		keyManager = GameObject.Find("GameManager").GetComponent<KeysManager>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag.Equals("Player")){
+			//keyManager.GetKey();
+			AudioSource.PlayClipAtPoint(audio.clip,transform.position);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/KeysManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeysManager : MonoBehaviour {
+
+	public enum Keys {Red = 0, Green = 1, Blue = 2, Yellow = 3};
+
+	public bool HasRedKey {get; set;}
+	public bool HasBlueKey {get; set;}
+	public bool HasGreenKey {get; set;}
+	public bool HasYellowKey {get; set;}
+
+	// Use this for initialization
+	void Start () {
+		HasRedKey = false;
+     	HasBlueKey = false;
+     	HasGreenKey = false;
+     	HasYellowKey = false;
+	}
+
+}
