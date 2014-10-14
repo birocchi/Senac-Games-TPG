d9dafa1afcac3d5afdd30600c48dce3f178c7f8e
lfadelli <leandro.fadelli@hotmail.com>
2014-10-14 17:57:30 -0300

    - Script desaparece ao ser pego.

--- DeveloperSaga/Assets/DeveloperSaga_Fase1/Scripts/ScriptTrigger.cs
+++ DeveloperSaga/Assets/DeveloperSaga_Fase1/Scripts/ScriptTrigger.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class ScriptTrigger : MonoBehaviour {
 	private PlayerController_Fase1 playerController;
+	Animator animator;
 	
 	void Start () {
 		playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController_Fase1>();
+		animator = this.GetComponent<Animator>();
 	}
 
 	
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag.Equals ("Player")  ) {
+		if (other != null && other.tag.Equals ("Player")  ) {
+			Destroy(this.collider2D);
+			animator.SetBool("ScriptTaken", true);
 			this.ApplyScript ();
 		}
 	}
