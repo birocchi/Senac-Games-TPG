ccd67c48c7ed183113ddca68725a23c7296c13d1
Anderson Birocchi <anderson.birocchi@gmail.com>
2014-10-17 00:22:24 -0300

    Bugs de camera + moeda -> bracket + boss song

---
+++ DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/ChangeFollowCameraBounds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFollowCameraBounds : MonoBehaviour {
+
+	public FollowObject followingCamera;
+	public float newLowerLimit;
+
+	void OnTriggerEnter2D(Collider2D other){
+		followingCamera.minLimit.y = newLowerLimit;
+	}
+}
---
+++ DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/ChangeMusic.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMusic : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		SongController.songToPlay = 1;
+	}
+}
