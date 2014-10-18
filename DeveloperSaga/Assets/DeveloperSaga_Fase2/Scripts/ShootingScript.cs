9a133dfd4da22aae44783aec6b462c1cac4da360
Anderson Birocchi <anderson.birocchi@gmail.com>
2014-10-17 22:25:50 -0300

    Nova animação do player da fase 2

--- DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/ShootingScript.cs
+++ DeveloperSaga/Assets/DeveloperSaga_Fase2/Scripts/ShootingScript.cs
@@ -7,9 +7,14 @@ public class ShootingScript : MonoBehaviour {
 	public float fireRate = 1;
 	public float shotSpeed = 7;
 	public float shotLifeTime = 0.6f;
+	public float armUpTime = 1.2f;
+
+	[HideInInspector]
+	public bool isShooting;
 	
 	private float fireInterval;
 	private float elapsedTime;
+	private float armUpCounter;
 	private GameObject shotInstance;
 	
 	void Start () {
@@ -19,15 +24,25 @@ public class ShootingScript : MonoBehaviour {
 
 	void Update () {
 		if(Input.GetButtonDown("Fire1") && elapsedTime >= fireInterval){
-			shotInstance = (GameObject)Instantiate(shot, transform.position, transform.rotation);
+			isShooting = true;
+			shotInstance = (GameObject)Instantiate(shot, transform.FindChild("ShotSpawn").position, transform.rotation);
 			shotInstance.GetComponent<ShotController>().Initialize(shotSpeed, transform.rotation * (Vector3)Vector2.right, shotLifeTime );
 			Physics2D.IgnoreCollision(this.collider2D, shotInstance.collider2D);
 			elapsedTime = 0;
+			armUpCounter = armUpTime;
 		}
 
 		if(elapsedTime < fireInterval){
-			elapsedTime += Time.fixedDeltaTime;
+			elapsedTime += Time.deltaTime;
+		}
+
+		if(armUpCounter <= 0){
+			isShooting = false;
+		}else{
+			armUpCounter -= Time.deltaTime;
 		}
 	}
+
+
 	
 }
